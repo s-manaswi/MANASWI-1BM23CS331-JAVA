469fa1a464c6a4198310e23d6508d3ab0f9507d3
s-manaswi <siripurapu.cs23@bmsce.ac.in>
2024-11-28 10:20:36 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,77 +1,35 @@
-import java.util.Scanner;
-abstract class Shape 
-{
-	float dim1, dim2;
-	Shape() {}		
-	abstract void printArea();
+class BMSCollegeThread extends Thread {
+    public void run() {
+        while (true) {
+            try {
+                System.out.println("BMS College of Engineering");
+                Thread.sleep(10000); 
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+    }
 }
 
-class Rectangle extends Shape 
-{
-	Rectangle() {}
-	void getd()
-	{
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter length of rectangle:");
-		dim1=sc.nextFloat();
-		System.out.println("Enter breadth of rectangle:");
-		dim2=sc.nextFloat();
-	}
-
-	void printArea() 
-	{
-        	double area = dim1 * dim2;
-        	System.out.println("Area of Rectangle: " + area);
-    	}
+class CSEThread extends Thread {
+    public void run() {
+        while (true) {
+            try {
+                System.out.println("CSE");
+                Thread.sleep(2000); 
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+    }
 }
 
-class Triangle extends Shape 
-{
-	Triangle() {}	
-	void getd()
-	{
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter height of triangle:");
-		dim1=sc.nextFloat();
-		System.out.println("Enter base of triangle:");
-		dim2=sc.nextFloat();
-	}
-	void printArea() 
-	{
-        	double area = 0.5 * dim1 * dim2;
-        	System.out.println("Area of Triangle: " + area);
-    	}
+public class Main {
+    public static void main(String[] args) {
+        BMSCollegeThread bmsThread = new BMSCollegeThread();
+        CSEThread cseThread = new CSEThread();
+        bmsThread.start();
+        cseThread.start();
+    }
 }
 
-class Circle extends Shape 
-{
-    Circle() {}
-	void getd()
-	{
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter radius of circle:");
-		dim1=sc.nextFloat();
-		dim2=0.0f;
-	}
-	void printArea() 
-	{
-        	double area = Math.PI * dim1 * dim1;
-        	System.out.println("Area of Circle: " + area);
-    	}
-}
-
-class Main
-{
-    public static void main(String[] args) 
-	{
-        	Rectangle rect = new Rectangle();
-        	Triangle tri = new Triangle();
-        	Circle circ = new Circle();
-		rect.getd();
-		tri.getd();
-		circ.getd();
-        	rect.printArea();
-        	tri.printArea();
-        	circ.printArea();
-    	}
-}
