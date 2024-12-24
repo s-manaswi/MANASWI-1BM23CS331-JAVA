a2250cb8442c4ffdb21cb5a19c7db0e01e022c11
s-manaswi <siripurapu.cs23@bmsce.ac.in>
2024-12-24 09:25:16 +0530

    Add files via upload

---
+++ Internal.java
@@ -0,0 +1,15 @@
+package CIE;
+import java.util.Scanner;
+public class Internal {
+	public int marksCie[] = new int[5];
+	public void getMarks() {
+		for(int i=0;i<5;i++) {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter CIE marks in subject "+(i+1));
+			marksCie[i]=sc.nextInt();
+		}
+	}
+	public int returnCieMarks(int i) {
+		return marksCie[i];
+	}
+}
